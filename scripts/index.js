6f9f554ed3dd553a1af9c93738b0d66d59e21b6e
vidhu20jan <vidhu20jan@gmail.com>
2024-05-13 17:47:39 +0530

    basic calc

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+function addby() {
+    num1 = document.getElementById(
+        "firstNumber").value;
+    num2 = document.getElementById(
+        "secondNumber").value;
+    document.getElementById(
+        "result").innerHTML = num1 + num2;
+}
+function subtractbyBy() {
+    num1 = document.getElementById(
+        "firstNumber").value;
+    num2 = document.getElementById(
+        "secondNumber").value;
+    document.getElementById(
+        "result").innerHTML = num1 - num2;
+}
+function multiplyBy() {
+    num1 = document.getElementById(
+        "firstNumber").value;
+    num2 = document.getElementById(
+        "secondNumber").value;
+    document.getElementById(
+        "result").innerHTML = num1 * num2;
+}
+function divideBy() {
+    num1 = document.getElementById(
+        "firstNumber").value;
+    num2 = document.getElementById(
+        "secondNumber").value;
+    document.getElementById(
+        "result").innerHTML = num1 / num2;
+}
